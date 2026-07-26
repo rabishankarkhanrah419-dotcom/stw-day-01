e5696fed6898834bf5046d36a75cfaed07906ce7
Rabishankar <rabishankarkhanrah419@gamil.com>
2026-07-26 18:02:39 +0530

    day 01 draft, js intro

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("Hello, World");
+console.log(21); 
+console.log("The value of 32 is:"+32);
+console.log(5==0);
+console.log(4+7);
+console.log(`Hare Krishna:
+               This is Gurukulam's STW`);
+console.log('5'+'3');
+console.log('5'-'3');
+console.log('5' == 5);//simply compare it
+console.log('5' === 5);//strictly equality operator
+console.log(`This is a text, this is the sum of 2+3: ${2+3}`);
+const name = "Rabishankar";
+console.log('Hey!',name,'how are you?');
+let a=5;
+let b=3;
+console.log(a+b);
+a=10;
+console.log(a+10);
+console.log(typeof a);
+console.log(typeof name);
+a = "Ayush";
+// name = 123;// It will give error
+const arr = [2,4,6,7,9];
+console.log(arr);
+const input=prompt("Enter yoyr name:");
+console.log("Your name is:",input);
+console.log(typeof arr);
+const obj={
+    name:{
+        first:"Rabishankar",
+        second:"Khanrah"},
+    age :21,
+    roll:11200125044,
+    isStudent: true
+
+}
+
+console.log(typeof obj === typeof arr);
+
+const abc =null;
+const xyz = undefined;
+console.log(abc===xyz);
+
+console.log(obj.name.first);
+console.log(obj.school);
